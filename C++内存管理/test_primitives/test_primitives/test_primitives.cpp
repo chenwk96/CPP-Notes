e53e067a92547b3a0d8c9eb308af4cacf9d51840
chenwk96 <wk_chen96@foxmail.com>
2021-06-09 11:09:27 +0800

    添加侯捷相关课程

---
+++ C++内存管理/test_primitives/test_primitives/test_primitives.cpp
@@ -0,0 +1,166 @@
+﻿#include <iostream>
+#include <string>
+//#include <memory>				 //std::allocator  
+using namespace std;
+
+//namespace jj02
+//{
+//
+//	class A
+//	{
+//	public:
+//		int id;
+//
+//		A() : id(0) { cout << "default ctor. this=" << this << " id=" << id << endl; }
+//		A(int i) : id(i) { cout << "ctor. this=" << this << " id=" << id << endl; }
+//		~A() { cout << "dtor. this=" << this << " id=" << id << endl; }
+//	};
+//
+//	void test_call_ctor_directly()
+//	{
+//		cout << "\ntest_call_ctor_directly().......... \n";
+//
+//		string* pstr = new string;
+//		cout << "str= " << *pstr << endl;
+//		//! pstr->string::string("jjhou");  
+//		//[Error] 'class std::basic_string<char>' has no member named 'string'
+//		//! pstr->~string();	//crash -- 其語法語意都是正確的, crash 只因為上一行被 remark 起來嘛.  
+//		cout << "str= " << *pstr << endl;
+//
+//
+//		//------------
+//
+//		A* pA = new A(1);         	//ctor. this=000307A8 id=1
+//		cout << pA->id << endl;   	//1
+//		pA->A::A(3);
+//		cout << pA->id << endl;
+//		//!	pA->A::A(3);                //in VC6 : ctor. this=000307A8 id=3
+//		//in GCC : [Error] cannot call constructor 'jj02::A::A' directly
+//
+//		A::A(5);
+//		//!	A::A(5);	  				//in VC6 : ctor. this=0013FF60 id=5
+//		//         dtor. this=0013FF60  	
+//		//in GCC : [Error] cannot call constructor 'jj02::A::A' directly
+//		//         [Note] for a function-style cast, remove the redundant '::A'
+//
+//		cout << pA->id << endl;   	//in VC6 : 3
+//		//in GCC : 1  	
+//
+//		delete pA;                	//dtor. this=000307A8 
+//
+//		//simulate new
+//		void* p = ::operator new(sizeof(A));
+//		cout << "p=" << p << endl; 	//p=000307A8
+//		pA = static_cast<A*>(p);
+//		pA->A::A(2);
+//		//!	pA->A::A(2);				//in VC6 : ctor. this=000307A8 id=2
+//		//in GCC : [Error] cannot call constructor 'jj02::A::A' directly  	
+//
+//		cout << pA->id << endl;     //in VC6 : 2
+//		//in GCC : 0  	
+//
+//		//simulate delete
+//		pA->~A();					//dtor. this=000307A8 
+//		::operator delete(pA);		//free()
+//	}
+//} //namespace
+//
+//int main(void)
+//{
+//	jj02::test_call_ctor_directly();
+//	return 0;
+//}
+
+
+namespace jj06
+{
+
+	class Foo
+	{
+	public:
+		int _id;
+		long _data;
+		string _str;
+
+	public:
+		static void* operator new(size_t size);
+		static void  operator delete(void* deadObject, size_t size);
+		static void* operator new[](size_t size);
+		static void  operator delete[](void* deadObject, size_t size);
+
+		Foo() : _id(0) { cout << "default ctor. this=" << this << " id=" << _id << endl; }
+		Foo(int i) : _id(i) { cout << "ctor. this=" << this << " id=" << _id << endl; }
+		//virtual 
+		~Foo() { cout << "dtor. this=" << this << " id=" << _id << endl; }
+
+		//不加 virtual dtor, sizeof = 12, new Foo[5] => operator new[]() 的 size 參數是 64, 
+		//加了 virtual dtor, sizeof = 16, new Foo[5] => operator new[]() 的 size 參數是 84, 
+		//上述二例，多出來的 4 可能就是個 size_t 欄位用來放置 array size. 
+	};
+
+	void* Foo::operator new(size_t size)
+	{
+		Foo* p = (Foo*)malloc(size);
+		cout << "Foo::operator new(), size=" << size << "\t  return: " << p << endl;
+
+		return p;
+	}
+
+	void Foo::operator delete(void* pdead, size_t size)
+	{
+		cout << "Foo::operator delete(), pdead= " << pdead << "  size= " << size << endl;
+		free(pdead);
+	}
+
+	void* Foo::operator new[](size_t size)
+	{
+		Foo* p = (Foo*)malloc(size);  //crash, 問題可能出在這兒 
+		cout << "Foo::operator new[](), size=" << size << "\t  return: " << p << endl;
+
+		return p;
+	}
+
+		void Foo::operator delete[](void* pdead, size_t size)
+	{
+		cout << "Foo::operator delete[](), pdead= " << pdead << "  size= " << size << endl;
+
+		free(pdead);
+	}
+
+	//-------------	
+	void test_overload_operator_new_and_array_new()
+	{
+		cout << "\ntest_overload_operator_new_and_array_new().......... \n";
+
+		cout << "sizeof(Foo)= " << sizeof(Foo) << endl;
+
+		{
+			Foo* p = new Foo(7);
+			delete p;
+
+			Foo* pArray = new Foo[5];	//無法給 array elements 以 initializer 
+			delete[] pArray;
+		}
+
+		{
+			cout << "testing global expression ::new and ::new[] \n";
+			// 這會繞過 overloaded new(), delete(), new[](), delete[]() 
+			// 但當然 ctor, dtor 都會被正常呼叫.  
+
+			Foo* p = ::new Foo(7);
+			::delete p;
+
+			Foo* pArray = ::new Foo[5];
+			::delete[] pArray;
+		}
+	}
+} //namespace
+
+int main(void)
+{	
+		cout << sizeof(int) << endl; 
+		cout << sizeof(long) << endl;
+		cout << sizeof(string) << endl;
+	jj06::test_overload_operator_new_and_array_new();
+	return 0;
+}
---
+++ C++面对对象高级编程/C++ 面向对象高级编程.md
@@ -0,0 +1,176 @@
+# C++ 面向对象高级编程
+
+侯捷C++面向对象高级编程课程过程中的一些记录，不能称之为笔记。
+
+B站课程连接：[C++面向对象高级编程(上)-基于对象＆面向对象_哔哩哔哩_bilibili](https://www.bilibili.com/video/BV1Lb4y1R7fs?p=3&spm_id_from=pageDriver)
+
+
+
+## C++编程简介
+
+
+
+## 头文件与类的声明
+
+
+
+## 构造函数
+
+1. 变量的初始化、赋值
+
+2. 不带指针的类多半不用写析构函数
+
+3. 函数重载，名称相同的函数在经编译器编译后，实际名称取决于编译器
+
+4. 构造函数可以放在private区，比如：单例模式（singleton），外界只能用一个
+
+   ```c++
+   class A {
+   public:
+       static A& getInstance();
+       setup() { ... }
+   private:
+       A();
+       A(const A& rhs);
+       ...
+   };
+   
+   A& A::getInstance() {
+   	static A a;
+       return a;
+   }
+   
+   // A::getInstance().setup();
+   ```
+
+   
+
+
+
+## 常量成员函数
+
+1. 凡是有可能打破const的行为都是禁止的
+
+2. 成员函数参数列表之后的const被称为成员函数限定符，其作用是将成员函数隐藏的this指针变为了常量指针
+
+   >常量指针表示一个指针指向的变量为常量 `const int *p`
+   >
+   >指针常量表示该指针自身是一个常量，不可改变，始终指向同一个地址，在定义时必须初始化
+   >
+   >```
+   >int a = 10, b = 20;
+   >int *const p = &a;
+   >*p = 30; // a = 30
+   >```
+
+
+
+## 参数传递：pass by value vs. pass by conference(to const)
+
+1. 尽量不用pass by value
+2. 传入的值不希望被改变的话就pass by conference to const
+
+
+
+## 返回值：return by value vs. return by conference(to const)
+
+1. 尽量return by conference
+2. 不要返回局部变量的引用
+3. 传递者无需知道接收者是以reference接收
+
+##  友元
+
+1. 自由取得friend的private成员
+
+2. 相同class的各个objects互为friends
+
+   > 自己原来根本没考虑过这个问题，比如类的成员函数以访问该类的另一个对象为参数，并且访问其私有成员
+
+
+
+## 操作符重载
+
+1. C++中操作符就是一种函数
+2. 两种情况：成员函数和非成员函数
+
+
+
+## temp object（临时对象） typename()
+
+
+
+## 三大函数（big three）：拷贝构造、拷贝复制、析构
+
+1. 没有自己编写的话，编译器会自己生成默认的，简单的不带指针的默认的一般就够用
+
+2. 在string的构造函数中，侯捷老师讲的好像有点问题，只能在默认函数的声明中定义默认参数的值而不能同时在构造函数的定义中也声明
+
+3. class with pointer members 必须有 **copy ctor** 和 **copy op=**
+
+4. 浅拷贝 & 深拷贝
+
+5. 拷贝赋值时需要先把自己指向的内容释放 
+
+6. ==检测自我赋值（self assignment）==
+
+   ![](https://i.loli.net/2021/06/08/ax94PZk5uRDJAfv.png)
+
+
+
+## 所谓stack（栈），所谓heap（堆）
+
+1. static local objects的生命在作用域结束后任然存在，直到整个程序结束
+
+2. global object的生命在整个程序结束之后才结束，其作用域是整个程序
+
+3. heap object的生命在它被deleted之际结束
+
+   ```C++ 
+   class Complex {...}
+   ...
+   {
+   	Complex* p = new Complex;
+   }
+   // 会出现内存泄漏（memory leak）
+   // 指针p的生命在作用域结束时结束，但p所指的heap object
+   // 仍然存在，作用域之外再也看不到p，也就没有机会delete p
+   ```
+
+4. new  先分配内存，在调用ctor； delete先调用dtor，再释放内存
+
+5. 动态分配内存块
+
+6. 内存对齐
+
+7. 指针的大小
+
+   
+
+## String类
+
+1. null, 0, nullptr
+2. 链式编程 `return *this`
+
+
+
+## 扩展补充：类模板、函数模板、及其他
+
+1. static成员，属于类的，而不是对象。静态成员函数没有this指针，只能处理静态成员变量
+
+2. 静态成员变量一定要在类的外面定义（变量获得内存）
+
+3. 调用静态函数的方式有两种：1. 通过对象调用；2. 通过class name 调
+
+4. ==单例设计模式==
+
+   <img src="https://i.loli.net/2021/06/09/kXQCpG1c52ISPhZ.png" alt="饿汉" style="zoom: 80%;" />
+
+   <img src="https://i.loli.net/2021/06/09/QASosVTczaku3Ux.png" alt="懒汉" style="zoom: 80%;" />
+
+5. namespace
+
+
+
+## 组合和继承
+
+1. 复合，表示has a
